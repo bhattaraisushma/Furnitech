3e5ff119cca55a5effd1b48b7862ceae7bc214de
bhattaraisushma <bhattaraisushma260@gmail.com>
2025-01-15 21:18:52 +0545

    login api

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,40 @@
-import express from 'express'
-import { connect } from 'mongoose';
-import { connectDB } from './db.js';
-import  saveUsertoDB from './Schema/Signup/AddsignuptoDB.js';
-import cors from 'cors';
-const app= express();
-const port=5000;
+import express from "express";
+import { connect } from "mongoose";
+import { connectDB } from "./db.js";
+import saveUsertoDB from "./Schema/Signup/AddsignuptoDB.js";
+import cors from "cors";
+import newUser from "./Schema/Signup/Schema.js";
+const app = express();
+const port = 5000;
 
 app.use(cors());
-app.listen(port,(req,res)=>{   
-    console.log(`Server is running on port ${port}`);
- }
-)
+app.listen(port, (req, res) => {
+  console.log(`Server is running on port ${port}`);
+});
 connectDB();
 app.use(express.json());
 
-app.post('/signupDetails',(req,res)=>{
- const {name,email,password}=req.body
- console.log(req.body)
-    // res.json(req.body, "from server")
-    res.json(req.body)
-    try{
-        saveUsertoDB (name,email,password)
-        
-    }
-    catch(err){
-        console.log(err)
-    }   
-  
-  
+app.post("/signupDetails", (req, res) => {
+  const { name, email, password } = req.body;
+  console.log(req.body);
+  // res.json(req.body, "from server")
+  res.json(req.body);
+  try {
+    saveUsertoDB(name, email, password);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
+app.get("/loginDetails", async (req, res) => {
+  try {
+    const allUsers = await newUser.find({});
 
-})
+    console.log(JSON.stringify(allUsers, null, 2));
+    res.json(allUsers);
+    
+  } catch (err) {
+    console.error("Error fetching users:", err);
+    res.status(500).json({ error: "An error occurred while fetching users" });
+  }
+});
